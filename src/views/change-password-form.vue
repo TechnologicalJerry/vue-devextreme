fcfb38f0652921d362b90900435aadc8375ab6f1
technologicaljerry <cserajatverma@gmail.com>
2023-04-05 22:39:38 +0530

    Update change-password-form.vue

--- src/views/change-password-form.vue
+++ src/views/change-password-form.vue
@@ -1,41 +1,27 @@
 <template>
   <form @submit.prevent="onSubmit">
     <dx-form :form-data="formData" :disabled="loading">
-      <dx-item
-        data-field="password"
-        editor-type="dxTextBox"
-        :editor-options="{ stylingMode: 'filled', placeholder: 'Password', mode: 'password' }"
-      >
+      <dx-item data-field="password" editor-type="dxTextBox"
+        :editor-options="{ stylingMode: 'filled', placeholder: 'Password', mode: 'password' }">
         <dx-required-rule message="Password is required" />
         <dx-label :visible="false" />
       </dx-item>
-      <dx-item
-        data-field="confirmedPassword"
-        editor-type="dxTextBox"
-        :editor-options="{ stylingMode: 'filled', placeholder: 'Confirm Password', mode: 'password' }"
-      >
+      <dx-item data-field="confirmedPassword" editor-type="dxTextBox"
+        :editor-options="{ stylingMode: 'filled', placeholder: 'Confirm Password', mode: 'password' }">
         <dx-required-rule message="Password is required" />
-        <dx-custom-rule
-          message="Passwords do not match"
-          :validation-callback="confirmPassword"
-        />
+        <dx-custom-rule message="Passwords do not match" :validation-callback="confirmPassword" />
         <dx-label :visible="false" />
       </dx-item>
       <dx-button-item>
-        <dx-button-options
-          width="100%"
-          type="default"
-          template="changePassword"
-          :use-submit-behavior="true"
-        >
+        <dx-button-options width="100%" type="default" template="changePassword" :use-submit-behavior="true">
         </dx-button-options>
       </dx-button-item>
 
       <template #changePassword>
         <div>
           <span class="dx-button-text">
-              <dx-loadIndicator v-if="loading" width="24px" height="24px" :visible="true" />
-              <span v-if="!loading">Continue</span>
+            <dx-loadIndicator v-if="loading" width="24px" height="24px" :visible="true" />
+            <span v-if="!loading">Continue</span>
           </span>
         </div>
       </template>
@@ -60,7 +46,7 @@ import { ref, reactive } from "vue";
 import auth from "../auth";
 
 export default {
-components: {
+  components: {
     DxForm,
     DxItem,
     DxLabel,
@@ -77,7 +63,7 @@ components: {
     const recoveryCode = ref("");
     const loading = ref(false);
     const formData = reactive({
-      password:""
+      password: ""
     });
 
     recoveryCode.value = route.params.recoveryCode;
@@ -85,10 +71,10 @@ components: {
     async function onSubmit() {
       const { password } = formData;
       loading.value = true;
-  
+
       const result = await auth.changePassword(password, recoveryCode.value);
       loading.value = false;
-  
+
       if (result.isOk) {
         router.push("/login-form");
       } else {
@@ -96,7 +82,7 @@ components: {
       }
     }
 
-    function confirmPassword (e) {
+    function confirmPassword(e) {
       return e.value === formData.password;
     }
 
@@ -110,6 +96,4 @@ components: {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
